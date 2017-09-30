5a6ef6128a50e8ba7b805d4788a77245c07fb5ea
Arun P <arunparamanathan@ksu.edu>
2017-09-30 14:35:24 -0500

    move player.
    move player.

--- breakout.js
+++ breakout.js
@@ -11,12 +11,19 @@
 // Canvas.
 var canvas = document.createElement('canvas');
 var ctx = canvas.getContext('2d');
+
 // Ball.
 var x = 375;
 var y = 450;
 var changeX = 0;
 var changeY = -1;
 var ballRadius = 10;
+
+// Player.
+var playerHeight = 15;
+var playerWidth = 80;
+var playerX = 333; // Close enough to center.
+var playerDirection = 'none'; // Start out stationary.
 
 // Create canvas and context.
 function setup()
@@ -36,11 +43,22 @@ function drawBall()
     ctx.closePath();
 }
 
-// Clear and draw new frame.
+// Draw player (will be used each frame).
+function drawPlayer()
+{
+    ctx.beginPath();
+    ctx.rect(playerX, canvas.height-playerHeight, playerWidth, playerHeight);
+    ctx.fillStyle = "black";
+    ctx.fill();
+    ctx.closePath();
+}
+
+// Clear old frame and draw new frame.
 function refreshFrame()
 {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     drawBall();
+    drawPlayer();
 
     // Collision detection with edge of ball and canvas.
     if(x + changeX > canvas.width - ballRadius || x + changeX < ballRadius) { changeX *= -1; }
@@ -48,6 +66,28 @@ function refreshFrame()
 
     x += changeX;
     y += changeY;
+
+    // Collision detection with player and edge of canvas.
+    // Also set player direction.
+    if(playerDirection === 'right' && playerX < canvas.width - playerWidth) { playerX += 2; }
+    else if(playerDirection === 'left' && playerX > 0) { playerX -= 2; }
+}
+
+// Listen for key press events and handle them.
+document.addEventListener("keydown", keyDownHandler, false);
+document.addEventListener("keyup", keyUpHandler, false);
+
+// Handle key down by changing direction if arrow keys are pressed.
+function keyDownHandler(e)
+{
+    if(e.keyCode === 39) { playerDirection = 'right'; }
+    else if(e.keyCode === 37) { playerDirection = 'left'; }
+}
+
+// If arrow keys are no longer pressed, stop player's motion.
+function keyUpHandler(e)
+{
+    if(e.keyCode === 39 || e.keyCode === 37) { playerDirection = 'none'; }
 }
 
 setup();
